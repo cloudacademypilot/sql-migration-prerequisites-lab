577e0366f9c14c3a3d8ae40ea147cf8c582339fa
Sameer Kulkarni <110413065+ksameer18@users.noreply.github.com>
2023-02-28 16:39:24 +0530

    Update validation.ps1

--- steps/2_performing-the-data-migration/checks/2_performed-the-data-migration/validation.ps1
+++ steps/2_performing-the-data-migration/checks/2_performed-the-data-migration/validation.ps1
@@ -1,1 +1,19 @@
-ToDo
+param(
+    [string]
+    $Resourcegroupname
+)
+
+if ((Get-Module -ListAvailable Az.DataMigration) -eq $null)
+	{
+       Install-Module -Name Az.DataMigration -RequiredVersion 0.7.4 -Force
+    }
+
+$dmsinstance = Get-AzResource -ResourceGroupName $Resourcegroupname -ResourceType Microsoft.DataMigration/services;
+$dmsname = $dmsinstance.Name;
+
+$datamigrationactivity = Get-AzDataMigrationTask -TaskName Datamigrationactivity -ServiceName $dmsname -ResourceGroupName $Resourcegroupname -ProjectName SqlToSqlDbMigrationProject -Expand;
+$result = $datamigrationactivity.ProjectTask.Properties.State;
+
+if ($result -eq "Succeeded") { 
+    Write-Host "data migration activity completed" }
+else { Write-Host "Please complete data migration activity" }
