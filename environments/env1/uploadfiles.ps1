6f053cd71f50164c0b5d964351b12e870c732fc6
vAyushkhare <110239762+vAyushkhare@users.noreply.github.com>
2023-03-03 14:46:20 +0530

    Update uploadfiles.ps1

--- environments/env1/uploadfiles.ps1
+++ environments/env1/uploadfiles.ps1
@@ -1,1 +1,3 @@
 Invoke-WebRequest 'https://raw.githubusercontent.com/CSLabsInternal/sql-migration-prerequisites-lab/main/environments/env1/SMF_DMAPreReqAssessCombo_V5.8.ps1' -OutFile .\SMF_DMAPreReqAssessCombo_V5.8.ps1
+Invoke-WebRequest 'https://raw.githubusercontent.com/CSLabsInternal/sql-migration-prerequisites-lab/main/environments/env1/DMA-INPUT-FILE.xlsx' -OutFile .\DMA-INPUT-FILE.xlsx
+Invoke-WebRequest 'https://raw.githubusercontent.com/CSLabsInternal/sql-migration-prerequisites-lab/main/environments/env1/Terminate.ps1' -OutFile .\Terminate.ps1
