97e32896dcdc9e3f60058a02f68a66f5519a8802
vAyushkhare <110239762+vAyushkhare@users.noreply.github.com>
2023-03-09 13:35:58 +0530

    Update AddDatabaseToSqlServer.ps1

--- environments/env1/AddDatabaseToSqlServer.ps1
+++ environments/env1/AddDatabaseToSqlServer.ps1
@@ -80,4 +80,7 @@ Restore-SqlDatabase `
 Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.201 -Force
 Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
 Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+Install-Module -Name Az.Accounts -Force
+Install-Module Az.Resources -Force
+Install-Module Az.Storage -Force
 Restart-Computer
