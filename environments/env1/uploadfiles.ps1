42e423c4ec5c1602c5fea3ce6ed18d18142e286e
Sameer Kulkarni <110413065+ksameer18@users.noreply.github.com>
2023-04-06 12:23:19 +0530

    Update uploadfiles.ps1

--- environments/env1/uploadfiles.ps1
+++ environments/env1/uploadfiles.ps1
@@ -1,6 +1,8 @@
 Invoke-WebRequest 'https://raw.githubusercontent.com/CSLabsInternal/sql-migration-prerequisites-lab/main/environments/env1/SMF_DMAPreReqAssessCombo_V5.8.ps1' -OutFile .\SMF_DMAPreReqAssessCombo_V5.8.ps1
 Invoke-WebRequest 'https://raw.githubusercontent.com/CSLabsInternal/sql-migration-prerequisites-lab/main/environments/env1/DMA-INPUT-FILE.xlsx' -OutFile .\DMA-INPUT-FILE.xlsx
 Invoke-WebRequest 'https://raw.githubusercontent.com/CSLabsInternal/sql-migration-prerequisites-lab/main/environments/env1/Terminate.ps1' -OutFile .\Terminate.ps1
+Invoke-WebRequest 'https://raw.githubusercontent.com/CSLabsInternal/sql-migration-prerequisites-lab/main/environments/env1/Terminate.ps1' -OutFile .\Network_Validation.ps1
+Invoke-WebRequest 'https://raw.githubusercontent.com/CSLabsInternal/sql-migration-prerequisites-lab/main/environments/env1/Terminate.ps1' -OutFile .\Network_Validation.xlsx
 Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.201 -Force
 Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
 Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
