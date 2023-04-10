6571059352d3db13e5bb1bafb6a1082508e5f402
Sameer Kulkarni <110413065+ksameer18@users.noreply.github.com>
2023-04-10 16:07:35 +0530

    Update validation.ps1

--- steps/2_dma-assessment-and-sku-recommendation/checks/dma-assessment-and-sku-recommendation/validation.ps1
+++ steps/2_dma-assessment-and-sku-recommendation/checks/dma-assessment-and-sku-recommendation/validation.ps1
@@ -1,1 +1,19 @@
+param(
+    [string]
+    $Resourcegroupname
+)
 
+$StorageAccountNames = Get-AzStorageAccount -ResourceGroupName $Resourcegroupname
+$StorageAccountName = $StorageAccountNames[0].StorageAccountName
+
+$key = (Get-AzStorageAccountKey -Resourcegroupname $Resourcegroupname -Name $StorageAccountName)[0].Value
+$context = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $key
+
+$Files = Get-AzStorageBlob -Container 'backup' -Context $context
+
+if($Files){
+    Write-Host "DMA Assessment step is completed"
+}
+else{
+    Write-Host "DMA Assessment step is not completed"
+}
