3dc6ed07f36b192ae1fca18c966675b7fd3cc4f4
lrakai <lmrakai@gmail.com>
2023-04-17 11:17:38 -0600

    target our GH for assets

--- environments/env1/adventure.ps1
+++ environments/env1/adventure.ps1
@@ -1,2 +1,2 @@
-Invoke-WebRequest 'https://raw.githubusercontent.com/CSLabsInternal/sql-migration-prerequisites-lab/main/environments/env1/adventure.zip' -OutFile .\adventure.zip
+Invoke-WebRequest 'https://raw.githubusercontent.com/cloudacademypilot/sql-migration-prerequisites-lab/main/environments/env1/adventure.zip' -OutFile .\adventure.zip
 Move-Item -Path C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.10.15\Downloads\0\adventure.zip -Destination C:\adventure.zip
